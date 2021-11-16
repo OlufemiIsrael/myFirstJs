1b3b4215b6bd0f24e9bc71e68e13d06695fbfec3
olufemi isarel <olufemiisraelpelumi@gmail.com>
2021-11-16 12:35:34 +0100

    my first javascript

---
+++ my.js
@@ -0,0 +1,20 @@
+document.getElementById("btn1").onclick= function() {
+     document.getElementById ("myImage").src="img/icons8-bulbon.png"
+}
+
+document.getElementById("btn").onclick= function() {
+    document.getElementById ("myImage").src="img/icons8-bulboff.png"
+}
+
+const person = {
+    firstName : "Israel",
+    lastName : "Olufemi",
+    age : "10",
+    DOB : 12_30_2011,
+    Details: function() {
+        return this.firstName +" "+ this.lastName +" "+ this.age +" "+ this.DOB
+    }
+}
+document.getElementById("btn2").onclick= function() {
+   return document.getElementById("demo").innerHTML = person.Details();
+}
